41047b390fa964e1adba25baba2503452289ad55
ljeb <jebarali@msu.edu>
2019-12-02 16:28:04 -0500

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -1,14 +1,6 @@
-import tweepy
 import json
-import operator
-import time
 import re
 from textblob import TextBlob
-from tweepy.streaming import StreamListener
-from tweepy import Stream
-from tweepy import OAuthHandler
-from tweepy import API
-from collections import defaultdict
 
 candidates = {'andrewYang': ['andrew', 'yang', '@andrewyang'],
               'amyKlobuchar': ['amy', 'klobuchar', '@amyklobuchar'], 
@@ -37,7 +29,7 @@ validLocations = ["AL", "Alabama", "AK", "Alaska", "AZ", "Arizona",
                   "PA", "Pennsylvania", "RI", "Rhode Island",
                   "SC", "South Carolina", "SD", "South Dakota", "TN", "Tennessee",
                   "TX", "Texas", "UT", "Utah", "VT", "Vermont", "VA", "Virginia",
-                  "WA", "Washington", "WV", "West Virginia", "WI", "Wisconsin", 
+                  "WA", "Washington", "WV", "West Virginia", "WI", "Wisconsin",  
                   "WY", "Wyoming", "U.S.A", 'United States']
 
 
@@ -46,26 +38,28 @@ def get_tweets(file_name):
     f = open(file_name)
     tweets = [json.loads(line) for line in f]
     f.close()
-    
-    filteredTweets = []
-    states = {validLocations[n]:[] for n in range(0, len(validLocations)-3, 2)}
+    filteredTweets = set()
+    states = {validLocations[n]: 0 for n in range(0, len(validLocations)-3, 2)}
 
     for tweet in tweets:
         if 'text' in list(tweet.keys())     \
         and 'retweeted_status' not in tweet \
         and tweet['user']['location']       \
         and any(loc in tweet['user']['location'] for loc in validLocations):
-            filteredTweets.append(tweet['text'])
+            filteredTweets.add(clean_tweet(tweet['text']))
             for loc in tweet['user']['location'].split(','):
                 if loc in validLocations[:-2]:
                     ind = validLocations.index(loc)
-                    if not ind%2:
-                        states[validLocations[ind]].append(clean_tweet(tweet['text']))
+                    if loc == "Washington" and tweet['user']['location'] != "Washington, DC":
+                        states['WA'] += 1
+                    elif tweet['user']['location'] == "Washington, DC":
+                        states['DC'] += 1 
+                    elif not ind%2:
+                        states[validLocations[ind]] += 1
                     else:
-                        states[validLocations[ind-1]].append(clean_tweet(tweet['text']))
+                        states[validLocations[ind-1]] += 1
+    return filteredTweets, states
 
-    return filteredTweets
-    
 def get_results(tweets):
     p = [tweet for tweet in tweets if get_tweet_sentiment(tweet) == 'positive']
     n = [tweet for tweet in tweets if get_tweet_sentiment(tweet) == 'negative']
@@ -83,13 +77,12 @@ def results(tweets):
     results.update({'total':get_results(tweets)})  
     return results
 
-def display_results(results):
+def display_results(results, states):
     print('Total results: '
           + '\n Positive tweet percentage: ' + str(results['total'][0])
           + '\n Negative tweet percentage: ' + str(results['total'][1])
           + '\n Neutral tweet percentage: '  + str(results['total'][2])
           + '\n')
-    
     for n in range(len(results.keys()) - 2):
         print(str(candidates[list(candidates.keys())[n]][0].capitalize()) + ' ' +
               str(candidates[list(candidates.keys())[n]][1].capitalize()) + 
@@ -97,23 +90,16 @@ def display_results(results):
               + '\n Negative tweet percentage: ' + str(results[list(results.keys())[n]][1])
               + '\n Neutral tweet percentage: '  + str(results[list(results.keys())[n]][2])
               + '\n')
+    print('State count:')
+    for state in states:
+        print(' Number of tweets from ' + str(state) + ': ' + str(states[state]))
                 
         
 def clean_tweet(tweet):
-    '''
-        Utility function to clean tweet text by removing links, special characters
-        using simple regex statements.
-        '''
     return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
 
 def get_tweet_sentiment(tweet):
-    '''
-        Utility function to classify sentiment of passed tweet
-        using textblob's sentiment method
-        '''
-    # create TextBlob object of passed tweet text
     analysis = TextBlob(clean_tweet(tweet))
-    # set sentiment
     if analysis.sentiment.polarity > 0:
         return 'positive'
     elif analysis.sentiment.polarity == 0:
@@ -121,6 +107,6 @@ def get_tweet_sentiment(tweet):
     else:
         return 'negative'
 
-tweets = get_tweets("first.json")
+tweets, states = get_tweets("first.json")
 results = results(tweets)
-display_results(results)
+display_results(results, states)
