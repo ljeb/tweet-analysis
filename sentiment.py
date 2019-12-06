75209e4df9ff93ff647ef92bbf33c7d9e70ee8b5
ljeb <jebarali@msu.edu>
2019-12-06 14:31:19 -0500

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -1,6 +1,9 @@
 import json
 import re
 from textblob import TextBlob
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+sid = SentimentIntensityAnalyzer()
 
 candidates = {'andrewYang': ['andrew', 'yang', '@andrewyang'],
               'amyKlobuchar': ['amy', 'klobuchar', '@amyklobuchar'], 
@@ -99,13 +102,12 @@ def clean_tweet(tweet):
     return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
 
 def get_tweet_sentiment(tweet):
-    analysis = TextBlob(clean_tweet(tweet))
-    if analysis.sentiment.polarity > 0:
-        return 'positive'
-    elif analysis.sentiment.polarity == 0:
-        return 'neutral'
-    else:
-        return 'negative'
+    analysis = TextBlob(tweet)
+    return 'positive' if analysis.sentiment.polarity > 0 else 'neutral' if analysis.sentiment.polarity == 0 else 'negative'
+    
+#def get_tweet_sentiment(tweet):
+#    analysis = sid.polarity_scores(tweet)
+#    return 'positive' if analysis['compound'] > 0 else 'neutral' if analysis['compound'] == 0 else 'negative'
 
 tweets, states = get_tweets("first.json")
 results = results(tweets)
